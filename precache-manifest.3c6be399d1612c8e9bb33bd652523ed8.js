dc624dda738a46448aeb13ff9fdc4726895b3877
TaniaTenorio <taniatenorio@hotmail.com>
2019-09-23 00:48:31 -0500

    Updates

---
+++ precache-manifest.3c6be399d1612c8e9bb33bd652523ed8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d9cd5970e1582f432e6ec34aef91460",
+    "url": "/MEX008-FE-Burger-Queen/index.html"
+  },
+  {
+    "revision": "784b9f27ce2d639dabbb",
+    "url": "/MEX008-FE-Burger-Queen/static/css/2.1e6f091f.chunk.css"
+  },
+  {
+    "revision": "ef24e7344817355122e8",
+    "url": "/MEX008-FE-Burger-Queen/static/css/main.16a20026.chunk.css"
+  },
+  {
+    "revision": "784b9f27ce2d639dabbb",
+    "url": "/MEX008-FE-Burger-Queen/static/js/2.d36f265a.chunk.js"
+  },
+  {
+    "revision": "ef24e7344817355122e8",
+    "url": "/MEX008-FE-Burger-Queen/static/js/main.8d3b05cf.chunk.js"
+  },
+  {
+    "revision": "1df6f6f19d1cb1f8397d",
+    "url": "/MEX008-FE-Burger-Queen/static/js/runtime~main.12b5020f.js"
+  },
+  {
+    "revision": "0c3813aaf78aa430f8d20db0ae3b8d74",
+    "url": "/MEX008-FE-Burger-Queen/static/media/logodemonium.0c3813aa.png"
+  }
+]);
