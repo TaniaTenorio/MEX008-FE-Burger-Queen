ec668ab17ab9725b87b9580116bae68161e3eace
TaniaTenorio <taniatenorio@hotmail.com>
2019-10-09 17:08:13 -0500

    Updates

---
+++ precache-manifest.6c2b49bc17fc03424555a82cef954673.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54e37eee704b064bce77fae192cc91c0",
+    "url": "/MEX008-FE-Burger-Queen/index.html"
+  },
+  {
+    "revision": "c06c3665b0e5b2643148",
+    "url": "/MEX008-FE-Burger-Queen/static/css/2.4a1d8450.chunk.css"
+  },
+  {
+    "revision": "68551a0cf85d9e38dee0",
+    "url": "/MEX008-FE-Burger-Queen/static/css/main.2df2821d.chunk.css"
+  },
+  {
+    "revision": "c06c3665b0e5b2643148",
+    "url": "/MEX008-FE-Burger-Queen/static/js/2.ff8abd3d.chunk.js"
+  },
+  {
+    "revision": "68551a0cf85d9e38dee0",
+    "url": "/MEX008-FE-Burger-Queen/static/js/main.9ad825f9.chunk.js"
+  },
+  {
+    "revision": "1df6f6f19d1cb1f8397d",
+    "url": "/MEX008-FE-Burger-Queen/static/js/runtime~main.12b5020f.js"
+  },
+  {
+    "revision": "3997785d568253ee608815aa1bf747f3",
+    "url": "/MEX008-FE-Burger-Queen/static/media/hot-dog.3997785d.png"
+  },
+  {
+    "revision": "25b4db2a32747c4eb716c9126e50369b",
+    "url": "/MEX008-FE-Burger-Queen/static/media/lemonade.25b4db2a.png"
+  },
+  {
+    "revision": "0c3813aaf78aa430f8d20db0ae3b8d74",
+    "url": "/MEX008-FE-Burger-Queen/static/media/logodemonium.0c3813aa.png"
+  },
+  {
+    "revision": "a5d80e4f5423b7cf7118c9edf4aa6e00",
+    "url": "/MEX008-FE-Burger-Queen/static/media/next.a5d80e4f.png"
+  },
+  {
+    "revision": "0a82d1f91a990842118de7c496ead5f9",
+    "url": "/MEX008-FE-Burger-Queen/static/media/offer.0a82d1f9.png"
+  },
+  {
+    "revision": "5906a54d5e32cb36405255f4f7576a04",
+    "url": "/MEX008-FE-Burger-Queen/static/media/pizzaicon.5906a54d.png"
+  },
+  {
+    "revision": "aa5b3e0301b785645472fc55c7b8774b",
+    "url": "/MEX008-FE-Burger-Queen/static/media/salad.aa5b3e03.png"
+  }
+]);
